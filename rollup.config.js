fe14629bf5160ce32ccddc31566ccb3ba1f71528
Andrej Sýkora <andrej@bohemian.studio>
2021-12-16 09:41:46 +0100

    update

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,10 @@
-import typescript from '@rollup/plugin-typescript'
 import copy from 'rollup-plugin-copy'
 import html from '@rollup/plugin-html'
 import styles from "rollup-plugin-styles";
+
+import typescript from "rollup-plugin-typescript2"
+import commonjs from "rollup-plugin-commonjs";
+import resolve from "rollup-plugin-node-resolve";
 
 export default {
     input: './src/index.ts',
@@ -10,9 +13,23 @@ export default {
         format: 'cjs',
     },
     plugins: [
-        typescript(),
+        resolve({
+            mainFields: ["module"], // Default: ["module", "main"]
+        }),
+
+        commonjs({
+            include: "node_modules/**"
+        }),
+
+        typescript({
+            typescript: require("typescript"),
+            tsconfig: "./tsconfig.json"
+        }),
+
         styles(),
+
         html({}),
+
         copy({
             targets: [
                 { src: 'vendor/fonts', dest: 'dist' }
--- src/index.ts
+++ src/index.ts
@@ -3,17 +3,29 @@ console.log('ťahať a púšťať')
 console.log('(c) 2021 andrej sýkora')
 console.log('===== ====== ====== ======')
 
+import { SVG } from '@svgdotjs/svg.js'
+
 import '../vendor/css/reset.css'
-import './gfx/styles.scss'
+import './gfx/styles.scss'  
 
 /**
  * Main function initializes the game
  */
 function main () {
+    const WINDOW_WIDTH = window.innerWidth
+    const WINDOW_HEIGHT = window.innerHeight
+
     const root = document.createElement('div')
     root.id = 'dd-root'
     root.style.width = '100vw'
     root.style.height = '100vh'
+
+    const svg = SVG().addTo(root).size(WINDOW_WIDTH, WINDOW_HEIGHT)
+    svg.rect(200, 110).move(100, 100).fill({
+        color: 'transparent'
+    }).stroke({
+        color: '#B2B2B2'
+    }).radius(5, 5)
 
     document.body.appendChild(root)
 }
